efed9275609fe77d08e9355c1428bc31ad72b74a
Ashutosh Khanal Upadhyay <ashutoshkhanal596@hotmail.com>
2025-04-19 12:56:51 +0545

    Replace welcome message with Hero component for enhanced visual presentation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- src/routes/+page.svelte -->
+<script>
+	import Hero from '$lib/components/Hero.svelte';
+</script>
+
+<div class="no-scrollbar overflow-auto">
+	<Hero />
+</div>
