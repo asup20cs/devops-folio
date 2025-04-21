68b7cc24bfeb0fb2389fc68a612d46f067488e5a
Ashutosh Khanal Upadhyay <ashutoshkhanal596@hotmail.com>
2025-04-21 16:39:23 +0545

    Remove mdsvex preprocessing from svelte.config.js to simplify configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 
 const config = {
 	kit: { adapter: adapter() },
-	preprocess: [mdsvex()],
+	preprocess: [],
 	extensions: ['.svelte', '.svx']
 };
 
